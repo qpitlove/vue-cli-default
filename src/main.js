2574537d2abb64afad956fcfcac93991134a2521
qpitlove <qpitlove@gmail.com>
2019-09-20 00:01:17 +0900

    env: vue add @uvue/ssr

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import createRouter from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export default () => {
+  const router = createRouter()
+  return new Vue({
+    router,
+    store,
+    render: h => h(App)
+  });
+}
